89bebd79c54bf34d6239716c47528bd77fcbb573
Rocky Pitto <rockypitto@gmail.com>
2019-10-04 16:06:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array<< "people"
+  add_element_to_end_of_array << "people"
 end
 
 def add_element_to_start_of_array(array, element)
