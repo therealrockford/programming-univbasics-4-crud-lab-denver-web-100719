3cfeb5fdb2fede819235ac7795e150819fd5f127
Rocky Pitto <rockypitto@gmail.com>
2019-10-04 16:08:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   ["water", "air", "earth", "fire"]
 end
 
-def add_element_to_end_of_array(create_an_array, "people")
-  create_an_array << "people"
+def add_element_to_end_of_array(array, element)
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
